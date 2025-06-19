5c2a38a6d81157139bbf0f44635d157d02e0a61b
Евгений Юхневич <myasom@list.ru>
2025-06-20 01:37:31 +0300

    1

---
+++ Components/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yuhnevich_vb_lab.Components
+{
+    public class CartViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,83 +1,36 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Yuhnevich_vb_lab.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: HomeController
-        public ActionResult Index()
-        {
+      
+            private readonly List<ListDemo> _listData;
+
+            public HomeController()
+            {
+                    _listData = new List<ListDemo>
+                    {
+                    new ListDemo{ Id=1, Name="Item 1"},
+                    new ListDemo{ Id=2, Name="Item 2"},
+                    new ListDemo{ Id=3, Name="Item 3"}
+                    };
+
+            }
+            public IActionResult Index()
+            {
+                
+                ViewData["Lst"] = new SelectList(_listData, "Id", "Name");
+            ViewData["Text"] = "Лабораторная работа 2";
             return View();
-        }
-
-        // GET: HomeController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: HomeController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: HomeController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
-        }
+     }
 
-        // GET: HomeController/Edit/5
-        public ActionResult Edit(int id)
+        public class ListDemo
         {
-            return View();
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
-
-        // POST: HomeController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
-}
+ }
